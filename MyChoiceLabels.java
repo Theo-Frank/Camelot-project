df876ad9759e422fc53e6ef739d0a0eb98fb5c4b
adequatechicken <166258089+adequatechicken@users.noreply.github.com>
2025-03-23 18:58:48 -0500

    Update MyChoiceLabels.java

--- MyChoiceLabels.java
+++ MyChoiceLabels.java
@@ -2,6 +2,7 @@ package myclassproject.mystorygraph;
 
 //Fill this enum with the labels you chose for the EDGES of your story graph.
 public enum MyChoiceLabels {
-	
+	TalkToKing, AcceptQuest, RejectQuest, ExitGreatHall, EnterCity, EnterTavern, TalkToBeggar, TalkToBartender;
+
 }
 
