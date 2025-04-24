32b86fcbb093ebe2b5a0b953e84458b084789575
adequatechicken <166258089+adequatechicken@users.noreply.github.com>
2025-04-24 13:44:41 -0500

    Update MyNodeLabels.java

--- MyNodeLabels.java
+++ MyNodeLabels.java
@@ -2,9 +2,9 @@ package myclassproject.mystorygraph;
 
 //Fill this enum with the labels you chose for the NODES of your story graph.
 public enum MyNodeLabels{
-	root, GreatHall,agreedToQuest, doNotTakeQuest,leaveGreatHall,enterCity, 
+	root, GreatHall, talkToKing, agreedToQuest, doNotTakeQuest,leaveGreatHall,enterCity, 
 	enterTavern,talkWithBartender,takeDrink,talkWithBeggar,enterForestPath,exitForestPath,
 	atForestPath,talkWithPriest,giveSwordToPriest,leaveToRuins,enterRuins,leaveRuins,
-	returnToForestPathFromRuins,killBandit,pickUpBlueKey,banditAttacksPlayer,death,takeRedKeyAndBag,atRuins,enterBridge,atBridge,exitBridge,answerRiddle,
+	returnToForestPathFromRuins, killBandit,pickUpBlueKey,banditAttacksPlayer,death,takeRedKeyAndBag,atRuins,enterBridge,atBridge,exitBridge,answerRiddle,
 	riddleAnswered,atPort,merchantInsists,buyKey,leavePort,talkWithKnight,questComplete,returnToThrone,leaveCityToBridge,returnToCityFromBridge//other nodes
 }
